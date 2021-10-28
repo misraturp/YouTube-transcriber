83e1e91491ac0c089947c452429b07dff2c4f51c
Mısra Turp <misraturp@Msras-MacBook-Pro.local>
2021-10-28 22:10:31 +0200

    updates to the code

--- youtube_transcriber.py
+++ youtube_transcriber.py
@@ -5,6 +5,9 @@ import pprint
 from configure import auth_key
 from time import sleep
 
+if 'status' not in st.session_state:
+    st.session_state['status'] = 'submitted'
+
 ydl_opts = {
    'format': 'bestaudio/best',
    'postprocessors': [{
@@ -26,7 +29,7 @@ headers = {
 }
 CHUNK_SIZE = 5242880
  
-
+@st.cache
 def transcribe_from_link(link, categories: bool):
    _id = link.strip()
 
@@ -37,9 +40,8 @@ def transcribe_from_link(link, categories: bool):
    meta = get_vid(_id)
    save_location = meta['id'] + ".mp3"
 
-   duration = meta['duration']
-
    print('Saved mp3 to', save_location)
+
 
    def read_file(filename):
        with open(filename, 'rb') as _file:
@@ -49,51 +51,54 @@ def transcribe_from_link(link, categories: bool):
                    break
                yield data
   
+
    upload_response = requests.post(
        upload_endpoint,
        headers=headers_auth_only, data=read_file(save_location)
    )
+
    audio_url = upload_response.json()['upload_url']
    print('Uploaded to', audio_url)
+
    transcript_request = {
        'audio_url': audio_url,
        'iab_categories': 'True' if categories else 'False',
    }
  
    transcript_response = requests.post(transcript_endpoint, json=transcript_request, headers=headers)
+
    transcript_id = transcript_response.json()['id']
    polling_endpoint = transcript_endpoint + "/" + transcript_id
 
    print("Transcribing at", polling_endpoint)
 
-   polling_response = requests.get(polling_endpoint, headers=headers)
+   return polling_endpoint
 
-   while polling_response.json()['status'] != 'completed':
-       sleep(30)
-       try:
-           polling_response = requests.get(polling_endpoint, headers=headers)
-       except:
-           print("Expected wait time:", duration*2/5, "seconds")
-           print("After wait time is up, call poll with id", transcript_id)
-           return transcript_id
 
-   _filename = transcript_id + '.txt'
-   
-   # with open(_filename, 'w') as f:
-   #     f.write(polling_response.json()['text'])
-   # print('Transcript saved to', _filename)
-   return polling_response.json()['text']
+def get_status(polling_endpoint):
+	polling_response = requests.get(polling_endpoint, headers=headers)
+	st.session_state['status'] = polling_response.json()['status']
+
+def refresh_state():
+	st.session_state['status'] = 'submitted'
 
 
 st.title('Easily transcribe YouTube videos')
 
 
-link = st.text_input('Enter your YouTube video link')
+link = st.text_input('Enter your YouTube video link', 'https://youtu.be/dccdadl90vs', on_change=refresh_state)
 st.video(link)
-transcript = transcribe_from_link(link, False)
 
-# with open('we0pvtvkm-645c-408b-ac03-c8c25acc6a8b.txt') as f:
-#     transcript = f.readlines()
+st.text("The transcription is " + st.session_state['status'])
+
+polling_endpoint = transcribe_from_link(link, False)
+
+st.button('check_status', on_click=get_status, args=(polling_endpoint,))
+
+transcript=''
+if st.session_state['status']=='completed':
+	polling_response = requests.get(polling_endpoint, headers=headers)
+	transcript = polling_response.json()['text']
 
 st.markdown(transcript)
 
